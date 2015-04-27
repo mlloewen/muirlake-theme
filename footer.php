f73c11fb9c8053e36ad382ef0ee3df7d9040d331
mlloewen <mlloewen@gmail.com>
2015-04-26 19:12:31 -0600

    mt

--- footer.php
+++ footer.php
@@ -7,17 +7,16 @@
  * @package Muir Lake
  */
 ?>
-
 	</div><!-- #content -->
-
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="container">
 			<div id="footer-content">
+				<?php do_shortcode('[social]');?>
 				<h2>Contact</h2>
 				<div id="contact">
 					<p><strong>Phone:</strong> 780.963.9975</p>
 					<p><strong>Email:</strong> <a href="mailto:info@muirlakealliance.ca" target="_blank">info@muirlakealliance.ca</a></p>
-					<p><strong>Face Book:</strong> <a href="https://www.facebook.com/pages/Muir-Lake-Community-Alliance-Church/1582120052007941?ref=bookmarks">Muir Lake Community Alliance Church</a></p>
+					<p><strong>Facebook:</strong> <a href="https://www.facebook.com/pages/Muir-Lake-Community-Alliance-Church/1582120052007941?ref=bookmarks">Muir Lake Community Alliance Church</a></p>
 					<p id="copyright-paragraph">© 2014 Muir Lake Alliance</p>	
 				</div><!-- #contact -->
 
